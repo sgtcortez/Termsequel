c015001f5d3eb1d39b5dab51b48aba047ddb72cc
Matheus Rambo <matheusrambo24@gmail.com>
2021-08-14 17:15:06 -0300

    Chore: Refactor compiler
    Create a class to act as Tokens. And, all possible token is an instance
    of this class.
    Rewrite the matches method, and, store it into the class

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -25,15 +25,15 @@
 
 namespace Termsequel {
 
-   enum TOKEN_TYPE {
+   enum TokenType {
       TYPE_COMMAND, // A SQL command, like SELECT
       TYPE_COLUMN, // An available column
       TYPE_IDENTIFIER, // An identifier
       TYPE_WHERE, // A restriction
       TYPE_COMMA, // A comma
       TYPE_FROM, // A target
-      TYPE_EQUAL, // equal
       TYPE_END, // Represents the end of the input
+      TYPE_COMPARASION, // Represents a comparasion
    };
 
    static constexpr const char * const TOKEN_MAP[] = {
@@ -43,51 +43,119 @@ namespace Termsequel {
       "WHERE",
       "COMMA",
       "FROM",
-      "EQUAL",
-      "END"
+      "END",
+      "COMPARASION"
    };
 
-   enum Token {
-      SELECT = TYPE_COMMAND,
-      FROM = TYPE_FROM,
-      WHERE = TYPE_WHERE,
-      NAME = TYPE_COLUMN,
-      SIZE = TYPE_COLUMN,
-      IDENTIFIER = TYPE_IDENTIFIER,
-      EQUAL = TYPE_EQUAL,
-      END = TYPE_END,
-      COMMA = TYPE_COMMA,
+   static constexpr char WHITE_SPACE = ' ';
+   static constexpr char COMMA       = ',';
+
+   // Tries to simulate java enums ...
+   class Token final {
+      private:
+         TokenType type;
+         Token(TokenType type) {
+            this->type = type;
+         };
+      public:
+
+         // These are static instances
+         static const Token SELECT;
+         static const Token NAME;
+         static const Token SIZE;
+         static const Token FROM;
+         static const Token IDENTIFIER;
+         static const Token WHERE;
+         static const Token COMMA;
+         static const Token END;
+         static const Token EQUAL;
+
+         bool is_end() const {
+            return type == TokenType::TYPE_END;
+         }
+
+         bool matches(Token next) const {
+            switch (type) {
+               case TokenType::TYPE_COMMAND:
+                  return ( next.type == TokenType::TYPE_COLUMN );
+               case TokenType::TYPE_COLUMN:
+                  return ( next.type == TokenType::TYPE_COMMA || next.type == TokenType::TYPE_FROM || next.type == TokenType::TYPE_COMPARASION );
+               case TokenType::TYPE_COMMA:
+                  return ( next.type == TokenType::TYPE_COLUMN );
+               case TokenType::TYPE_FROM:
+                  return ( next.type == TokenType::TYPE_IDENTIFIER);
+               case TokenType::TYPE_WHERE:
+                  return ( next.type == TokenType::TYPE_COLUMN );
+               case TokenType::TYPE_COMPARASION:
+                  return ( next.type == TokenType::TYPE_IDENTIFIER );
+               case TokenType::TYPE_IDENTIFIER:
+                  return ( next.type == TokenType::TYPE_WHERE || next.type == TokenType::TYPE_END );
+               default:
+                  // Must not enter here, otherwise, there is a problem with the compiler
+                  return false;
+            };
+         };
+
+         bool operator==(const Token &right) {
+            return type == right.type;
+         };
+
+         const char * name() const {
+            return TOKEN_MAP[type];
+         }
+
+
    };
+
+   // initializes the static instances.
+   // There are no need to free this ...
+   Token const Token::SELECT     = (TokenType::TYPE_COMMAND);
+   Token const Token::NAME       = (TokenType::TYPE_COLUMN);
+   Token const Token::SIZE       = (TokenType::TYPE_COLUMN);
+   Token const Token::FROM       = (TokenType::TYPE_FROM);
+   Token const Token::IDENTIFIER = (TokenType::TYPE_IDENTIFIER);
+   Token const Token::WHERE      = (TokenType::TYPE_WHERE);
+   Token const Token::COMMA      = (TokenType::TYPE_COMMA);
+   Token const Token::END        = (TokenType::TYPE_END);
+   Token const Token::EQUAL      = (TokenType::TYPE_COMPARASION);
 
    struct Lexeme {
 
-      enum Token token;
+      const Token *token;
       std::string *value;
 
       Lexeme (
-         enum Token token,
-         std::string string
+         const Token &token
       ) {
-         this->token = token;
-         value = new std::string(string);
+         this->token = &token;
+         value = nullptr;
       }
 
-      ~Lexeme() {
+      Lexeme (
+         const Token &token,
+         std::string identifier
+      ) {
+         this->token = &token;
+         this->value = new std::string(identifier);
+      }
+
+      ~Lexeme () {
          delete value;
       }
 
-      bool is_terminal() {
-         return token == Token::END;
+      bool is_terminal() const {
+         return token->is_end();
       };
 
       // TODO: Does not work without friend, and, I do not know why
       friend std::ostream & operator << (std::ostream &output_stream, const Lexeme &lexeme) {
-         output_stream << " Token: " << TOKEN_MAP[lexeme.token];
-         output_stream << " Value: " << *(lexeme.value);
+         output_stream << " Token: " << lexeme.token->name();
+         if (lexeme.value) output_stream << " Value: " << *(lexeme.value);
          return output_stream;
       }
 
    };
+
 
    class Lexical {
       private:
@@ -96,22 +164,22 @@ namespace Termsequel {
 
          Lexeme * parse_lexeme (const std::string string) {
             if (string.compare("SELECT") == 0 ) {
-               return new Lexeme( Token::SELECT, string );
+               return new Lexeme( Token::SELECT);
             } else if ( string.compare("NAME") == 0 ) {
-               return new Lexeme( Token::NAME, string);
+               return new Lexeme( Token::NAME);
             } else if ( string.compare("SIZE") == 0 ) {
-               return new Lexeme ( Token::SIZE, string );
+               return new Lexeme ( Token::SIZE );
             } else if ( string.compare("FROM") == 0 ) {
-               return new Lexeme ( Token::FROM, string );
+               return new Lexeme ( Token::FROM );
             } else if ( string.compare("WHERE") == 0 ) {
-               return new Lexeme (Token::WHERE, string);
+               return new Lexeme (Token::WHERE);
             } else if ( string.compare("=") == 0 ) {
-               return new Lexeme (Token::EQUAL, string);
+               return new Lexeme (Token::EQUAL);
             } else if ( string.compare(",") == 0 ) {
-               return new Lexeme ( Token::COMMA, string );
+               return new Lexeme ( Token::COMMA );
             } else {
                // anything else is an identifier
-               return new Lexeme(Token::IDENTIFIER, string);
+               return new Lexeme( Token::IDENTIFIER, string);
             }
          }
 
@@ -124,22 +192,22 @@ namespace Termsequel {
          Lexeme * next() {
 
             while (true) {
-               if ( current_index >= raw_input.size() || raw_input[current_index] != ' ' ) break;
+               if ( current_index >= raw_input.size() || raw_input[current_index] != WHITE_SPACE ) break;
                current_index++;
             }
             // No more things to parse, because, we already reached the end of the input
-            if ( current_index >= raw_input.size() ) return new Lexeme (Token::END,  "End of input");
-            auto tmp_index = raw_input.find(' ', current_index);
+            if ( current_index >= raw_input.size() ) return new Lexeme (Token::END);
+            auto tmp_index = raw_input.find(WHITE_SPACE, current_index);
             if ( tmp_index == std::string::npos ) {
                // Reached the end
                tmp_index = raw_input.size();
             }
             auto token_string = raw_input.substr(current_index, tmp_index - current_index);
-            const auto comma_index = token_string.find(",");
+            const auto comma_index = token_string.find(COMMA);
             if ( comma_index != std::string::npos ) {
                // there are commas appended with another value
 
-               if (token_string[0] == ',') {
+               if (token_string[0] == COMMA) {
                   // The first character is a comma, so, parse the comma
                   // and points to the next character
                   current_index++;
@@ -158,41 +226,6 @@ namespace Termsequel {
          };
     };
 
-    bool match(
-        Token token,
-        Token received
-    ) {
-      switch (token) {
-         case SELECT:
-               // SELECT NAME ...  OR SELECT SIZE ...
-               return (received == NAME || received == SIZE);
-         case NAME:
-               // since NAME and SIZE point to the same value, we do not need to handle them
-               // they both are columns
-               // SELECT NAME, ... OR SELECT NAME FROM ... OR SELECT ... WHERE NAME = ...
-               return (received == COMMA || received == FROM || received == EQUAL);
-         case COMMA:
-               // name and size point to the same value
-               // SELECT ..., NAME OR SELECT ..., SIZE
-               return (received == NAME || received == SIZE);
-         case FROM:
-               // SELECT ... FROM USER_DEFINED_VALUE
-               return (received == IDENTIFIER);
-         case WHERE:
-               // SELECT ... FROM IDENTIFIER WHERE NAME OR SELECT ... FROM IDENTIFIER WHERE SIZE
-               return (received == NAME || received == SIZE);
-         case EQUAL:
-               // SELECT ... WHERE NAME = ...
-               return (received == IDENTIFIER);
-         case IDENTIFIER:
-               // since the where is optional, the expected value from identifier can be the where and the end
-               // SELECT ... FROM USER_DEFINED_VALUE OR SELECT .. FROM USER_DEFINED_VALUE WHERE COLUMN = USER_DEFINED_VALUE
-               return (received == WHERE || received == END);
-         case END:
-               // THE END OF THE INPUT STREAM
-               return true;
-      };
-    };
 };
 
 Termsequel::Compiler::Compiler(std::string raw_input) {
@@ -212,62 +245,47 @@ void Termsequel::Compiler::execute() {
          std::cout << *lexeme << std::endl;
       #endif
       lexemes.push_back(lexeme);
-      if ((*lexeme).token == END) break;
+      if ((*lexeme).token->is_end()) break;
    }
 
    for (unsigned long index = 1L; index < lexemes.size(); index ++) {
       const auto previous = lexemes[index - 1];
       const auto current = lexemes[index];
-      if (!(match(previous->token, current->token))) {
+      if (!(previous->token->matches(*(current->token)))) {
          std::cerr << "Invalid SYNTAX!" << std::endl;
          std::cerr << "Near Tokens:" << *previous << " and " << *current << std::endl;
          has_errors = true;
          break;
       }
    }
+
    if (!(has_errors)) {
 
       auto system_command = Command();
 
-      // Converts to the operating system interface
-      for (auto element : lexemes) {
-         switch (element->token) {
-            case SELECT:
-                  system_command.command = COMMAND_TYPE::LIST;
-                  break;
-            case NAME:
-                  // name and size point to the same integer value
-                  if ( element->value->compare("NAME") == 0 ) {
-                     // name
-                     system_command.columns.push_back(COLUMN_TYPE::FILENAME);
-                  } else {
-                     // size
-                     system_command.columns.push_back(COLUMN_TYPE::FILESIZE);
-                  }
-                  break;
-            case IDENTIFIER:
-                  // target file/directory
-                  system_command.target = *(element->value);
-                  break;
-            case WHERE:
-                  break;
-            case EQUAL:
-                  break;
-            default:
-                  // does nothing
-                  break;
+      for (const auto lexeme: lexemes) {
 
-            // frees the memory
-            delete element;
+         auto token = *(lexeme)->token;
+         if (token == Token::SELECT) {
+            system_command.command = COMMAND_TYPE::LIST;
+         } else if ( token == Token::NAME ) {
+            system_command.columns.push_back(COLUMN_TYPE::FILENAME);
+         } else if ( token == Token::SIZE ) {
+            system_command.columns.push_back((COLUMN_TYPE::FILESIZE));
+         } else if ( token == Token::IDENTIFIER ) {
+            system_command.target = *(lexeme->value);
          }
       }
-      auto result = System::execute(&system_command);
-      for ( auto element : *result ) {
-         std::cout << *element << std::endl;
-         delete element;
+
+      const auto result_rows = System::execute(&system_command);
+      for (const auto row : *(result_rows)) {
+         std::cout << *(row) << std::endl;
+         delete row;
       }
-      delete result;
+      delete result_rows;
    }
+
+
 
    // delete the lexemes ...
    for(auto lexeme : lexemes) delete lexeme;
