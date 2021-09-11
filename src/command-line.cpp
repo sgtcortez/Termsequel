0e1e8c013d1481c3ef1bd82d6d41505199de0036
Matheus Rambo <matheusrambo24@gmail.com>
2021-09-11 12:59:52 -0300

    Fix: Implement custom getopt #88

---
+++ src/command-line.cpp
@@ -0,0 +1,19 @@
+#include "command-line.hpp"
+
+Termsequel::CommandLineParser::CommandLineParser(
+   char **argv,
+   int argc
+) {
+   for(int arg = 0; arg < argc; arg++) {
+      this->arguments.push_back(argv[arg]);
+   }
+   this->current = 0;
+}
+
+bool Termsequel::CommandLineParser::has_next() {
+   return current < arguments.size();
+}
+
+std::string Termsequel::CommandLineParser::parse() {
+   return arguments[current++];
+}
